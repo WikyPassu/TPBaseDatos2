6f63c9a0a72d628ebdc5918fd1610fad5b9a2d57
WikyPassu <alan_passucci@hotmail.com>
2021-06-19 00:33:26 -0300

    Fixed geoNear

--- controllers/sucursal.controller.js
+++ controllers/sucursal.controller.js
@@ -1,34 +1,43 @@
 const db = require('../models/db');
 
-//Empleados a 2km de la sucursal
-exports.traerEmpleadosA2km = (req, res) => {
-    db.getInstance().collection('sucursales').aggregate([
-        {
-            $geoNear: {
-                near: {
-                    type: "Point",
-                    coordinates: [-58.391009, -34.696036]
-                },
-                distanceField: "dist.calculated",
-                maxDistance: 2000
+//Clientes a 5km de la sucursal atencion al cliente
+exports.traerClientesA5kmAtencionAlCliente = (req, res) => {
+    db.getInstance().collection('sucursales').find({
+        descripcion: "atencion al cliente"
+    }).toArray().then((sucursal) => {
+        db.getInstance().collection('clientes').find(
+            {
+                "domicilio.geometry": {
+                    $near: {
+                        $geometry: sucursal[0].direccion.geometry,
+                        $maxDistance: 5000
+                    }
+                }
             }
-        },
-        {
-            $lookup: {
-                from: "empleados",
-                localField: "descripcion",
-                foreignField: "sucursal",
-                as: "empleados_2km"
+        ).toArray((err, result) => {
+            if (err) return console.log(err);
+            res.send(result);
+        });
+    });
+};
+
+//Empleados a 1km de la sucursal servicio tecnico
+exports.traerEmpleadosA1kmServicioTecnico = (req, res) => {
+    db.getInstance().collection('sucursales').find({
+        descripcion: "servicio tecnico"
+    }).toArray().then((sucursal) => {
+        db.getInstance().collection('empleados').find(
+            {
+                "domicilio.geometry": {
+                    $near: {
+                        $geometry: sucursal[0].direccion.geometry,
+                        $maxDistance: 1000
+                    }
+                }
             }
-        },
-        {
-            $project: {
-                empleados_2km: "$empleados_2km",
-                cantidad_empleados: { $size: "$empleados_2km" }
-            }
-        }
-    ]).toArray((err, result) => {
-        if (err) return console.log(err);
-        res.send(result);
+        ).toArray((err, result) => {
+            if (err) return console.log(err);
+            res.send(result);
+        });
     });
 };
--- routes/sucursal.js
+++ routes/sucursal.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const SucursalController = require('../controllers/sucursal.controller');
 
-router.get('/empleadosA2km', SucursalController.traerEmpleadosA2km);
+router.get('/clientesA5kmAtencionAlCliente', SucursalController.traerClientesA5kmAtencionAlCliente);
+router.get('/empleadosA1kmServicioTecnico', SucursalController.traerEmpleadosA1kmServicioTecnico);
 
 module.exports = router;
